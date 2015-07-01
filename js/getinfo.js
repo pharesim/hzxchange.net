f10e14d7c664d96624f707b591738b640cd021be
pharesim <pharesim@nhzcrypto.org>
2015-07-01 07:21:37 +0200

    js number_format

--- js/getinfo.js
+++ js/getinfo.js
@@ -16,54 +16,82 @@ function calculate(a,b,operator,cf){
 	}
 }
 
+function number_format(number, decimals, dec_point, thousands_sep) {
+	number = (number + '')
+    .replace(/[^0-9+\-Ee.]/g, '');
+  var n = !isFinite(+number) ? 0 : +number,
+    prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
+    sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
+    dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
+    s = '',
+    toFixedFix = function(n, prec) {
+      var k = Math.pow(10, prec);
+      return '' + (Math.round(n * k) / k)
+        .toFixed(prec);
+    };
+  // Fix for IE parseFloat(0.55).toFixed(0) = 0;
+  s = (prec ? toFixedFix(n, prec) : '' + Math.round(n))
+    .split('.');
+  if (s[0].length > 3) {
+    s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
+  }
+  if ((s[1] || '')
+    .length < prec) {
+    s[1] = s[1] || '';
+    s[1] += new Array(prec - s[1].length + 1)
+      .join('0');
+  }
+  return s.join(dec);
+}
+
 function getMainRow(result,coin) {
-	var change = (result.last-result.twefohr)*100/result.twefohr;
-	var diff = (result.ask-result.bid)*100/result.ask;
+	var change = calculate((result.last-result.twefohr),100,'*')/result.twefohr;
+	var diff = calculate((result.ask-result.bid),100,'*')/result.ask;
 	var string = '<tr>'+
 		'<td title="'+coin+'">'+
 			'<h2 class="h5style tabletext">'+result.name+'</h2>'+
 		'</td>'+
 		'<td style="text-align:right;">'+
 			'<h2 class="h5style tabletext">'+
-				'<div class="showCurrency inHZ">'+result.last+'</div>'+
+				'<div class="showCurrency inHZ">'+number_format(result.last,8-result.decimals)+'</div>'+
 				// '<div class="showCurrency inBTC">'+result['lastBTC']+'</div>'+
 			'</h2>'+
 		'</td>'+
 		'<td style="text-align:right;">'+
 			'<h2 class="h5style tabletext">'+
-				'<div class="showCurrency inHZ">'+result.bid+'</div>'+
+				'<div class="showCurrency inHZ">'+number_format(result.bid,8-result.decimals)+'</div>'+
 				// '<div class="showCurrency inBTC">'+result['bidBTC']+'</div>'+
 			'</h2>'+
 		'</td>'+
 		'<td style="text-align:right;">'+
 			'<h2 class="h5style tabletext">'+
-				'<div class="showCurrency inHZ">'+result.ask+'</div>'+
+				'<div class="showCurrency inHZ">'+number_format(result.ask,8-result.decimals)+'</div>'+
 				// '<div class="showCurrency inBTC">'+result['askBTC']+'</div>'+
 			'</h2>'+
 		'</td>'+
 		'<td style="text-align:right;">'+
-			'<h2 class="h5style tabletext">'+diff+'%</h2>'+
+			'<h2 class="h5style tabletext">'+number_format(diff,2)+'%</h2>'+
 		'</td>'+
 		'<td class="mobilehide" style="text-align:right;">'+
 			'<h2 class="h5style tabletext">'+
-				'<div class="showCurrency inHZ">'+result.high+'</div>'+
+				'<div class="showCurrency inHZ">'+number_format(result.high,8-result.decimals)+'</div>'+
 				// '<div class="showCurrency inBTC">'+result.highBTC+'</div>'+
 			'</h2>'+
 		'</td>'+
 		'<td class="mobilehide" style="text-align:right;">'+
 			'<h2 class="h5style tabletext">'+
-				'<div class="showCurrency inHZ">'+result.low+'</div>'+
+				'<div class="showCurrency inHZ">'+number_format(result.low,8-result.decimals)+'</div>'+
 				// '<div class="showCurrency inBTC">'+result.lowBTC+'</div>'+
 			'</h2>'+
 		'</td>'+
 		'<td class="mobilehide" style="text-align:right;">'+
 			'<h2 class="h5style tabletext">'+
-				'<div class="showCurrency inHZ">'+result.volume+'</div>'+
+				'<div class="showCurrency inHZ">'+number_format(result.volume,8-result.decimals)+'</div>'+
 				// '<div class="showCurrency inBTC">'+result.volumeBTC+'</div>'+
 			'</h2>'+
 		'</td>'+
 		'<td style="text-align:right;">'+
-			'<h2 class="h5style tabletext">'+change+'%</h2>'+
+			'<h2 class="h5style tabletext">'+number_format(change,2)+'%</h2>'+
 		'</td>'+
 		'<td id="'+result.name+'_actions" style="text-align:center;">'+
 			'<a href="#" class="btn btn-primary h5style details" data-toggle="modal" data-target="#'+
@@ -96,6 +124,7 @@ function getAssetInfo(id) {
 
 	var result = {
 		'name':asset.name,
+		'decimals':asset.decimals,
 		'volume':0,
 		'twefohr':0,
 		'last':0,
